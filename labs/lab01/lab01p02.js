fc66abbcc7d22865e6db36b7657b2621fc4f44de
birmansahil <contact.birmansahil@gmail.com>
2025-05-13 19:50:19 -0400

    added html pages for each endpoint and different messages for each method

---
+++ labs/lab01/lab01p02.js
@@ -0,0 +1,29 @@
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    const data = fs.readFileSync("./html/index.html");
+    res.end(data);
+  } else if (req.url === "/about") {
+    const data = fs.readFileSync("./html/about.html");
+    res.end(data);
+  } else if (req.url === "/contact") {
+    const data = fs.readFileSync("./html/contact.html");
+    res.end(data);
+  } else if (req.url === "/methods") {
+    if (req.method === "GET") {
+      res.end("Hello to the GET method");
+    } else if (req.method === "POST") {
+      res.end("Hello to the POST method");
+    } else if (req.method === "PUT") {
+      res.end("Hello to the PUT method");
+    }
+  } else {
+    res.end("404 page not found");
+  }
+});
+
+server.listen(8000, () => {
+  console.log(`http://localhost:8000`);
+});
